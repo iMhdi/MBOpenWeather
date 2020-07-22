9c1412acbe4e57ce3581e1eb3d6a521c5dc3ed79
m.boukhris@gmail.com <m.boukhris@gmail.com>
2020-07-23 00:48:30 +0200

    Return callbacks in main thread

--- MBOpenWeather/Classes/Utilities/MBRequestWrapper.swift
+++ MBOpenWeather/Classes/Utilities/MBRequestWrapper.swift
@@ -12,7 +12,10 @@ class MBRequestWrapper: NSObject {
     
     class func requestAPI<T:Codable>(withURL apiUrl: String?, method: String!, andParameters parameters: [String: String]?, success:@escaping (T) -> Void, failure:@escaping (NSError) -> Void) {
         guard let urlString = apiUrl else {
-            return failure(NSError(domain:"INVALID_URL", code:-1, userInfo:nil))
+            DispatchQueue.main.async {
+                return failure(NSError(domain:"INVALID_URL", code:-1, userInfo:nil))
+            }
+            return
         }
         
         switch method {
@@ -23,7 +26,10 @@ class MBRequestWrapper: NSObject {
             MBRequestWrapper.makePostApiCall(withURL: urlString, parameters: parameters, success: success, failure: failure)
             break;
         default:
-            return failure(NSError(domain:"UNSUPPORTED_METHOD", code:-2, userInfo:nil))
+            DispatchQueue.main.async {
+                return failure(NSError(domain:"UNSUPPORTED_METHOD", code:-2, userInfo:nil))
+            }
+            return
         }
     }
 }
@@ -49,21 +55,33 @@ extension MBRequestWrapper {
             
             guard let httpResponse = response as? HTTPURLResponse, let receivedData = data else {
                 print("ERROR: not a valid http response")
-                return failure(NSError(domain:"INVALID_RESPONSE", code:-3, userInfo:nil))
+                DispatchQueue.main.async {
+                    failure(NSError(domain:"INVALID_RESPONSE", code:-3, userInfo:nil))
+                }
+                return
             }
             
             switch (httpResponse.statusCode) {
             case 200:
                 do {
                     let successResponse = try JSONDecoder().decode(T.self, from: receivedData)
-                    return success(successResponse)
+                    DispatchQueue.main.async {
+                        success(successResponse)
+                    }
+                    return
                 } catch {
                     print("error serializing JSON: \(error)")
-                    return failure(error as NSError)
+                    DispatchQueue.main.async {
+                        failure(error as NSError)
+                    }
+                    return
                 }
             default:
                 print("GET request got response code \(httpResponse.statusCode)")
-                return failure(NSError(domain:"INVALID_RESPONSE_CODE", code:-4, userInfo:nil))
+                DispatchQueue.main.async {
+                    failure(NSError(domain:"INVALID_RESPONSE_CODE", code:-4, userInfo:nil))
+                }
+                return
             }
         }
         dataTask.resume()
@@ -85,21 +103,33 @@ extension MBRequestWrapper {
         let task = session.dataTask(with: request) { data, response, error in
             guard let httpResponse = response as? HTTPURLResponse, let receivedData = data else {
                 print("ERROR: not a valid http response")
-                return failure(NSError(domain:"INVALID_RESPONSE", code:-3, userInfo:nil))
+                DispatchQueue.main.async {
+                    return failure(NSError(domain:"INVALID_RESPONSE", code:-3, userInfo:nil))
+                }
+                return
             }
             
             switch (httpResponse.statusCode) {
             case 200:
                 do {
                     let successResponse = try JSONDecoder().decode(T.self, from: receivedData)
-                    return success(successResponse)
+                    DispatchQueue.main.async {
+                        success(successResponse)
+                    }
+                    return
                 } catch {
                     print("error serializing JSON: \(error)")
-                    return failure(error as NSError)
+                    DispatchQueue.main.async {
+                        return failure(error as NSError)
+                    }
+                    return
                 }
             default:
                 print("POST request got response code \(httpResponse.statusCode)")
-                return failure(NSError(domain:"INVALID_RESPONSE_CODE", code:-4, userInfo:nil))
+                DispatchQueue.main.async {
+                    return failure(NSError(domain:"INVALID_RESPONSE_CODE", code:-4, userInfo:nil))
+                }
+                return
             }
         }
         
